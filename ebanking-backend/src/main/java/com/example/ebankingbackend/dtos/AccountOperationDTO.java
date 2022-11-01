9107e8497d706023a72a027f96e9cd367137cd2d
tallahmad047 <tallahmad047@gmail.com>
2022-11-01 01:30:19 +0000

    dernier partie de la partie backand

---
+++ ebanking-backend/src/main/java/com/example/ebankingbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,22 @@
+package com.example.ebankingbackend.entities;
+
+import com.example.ebankingbackend.enums.OperationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private Date operationDate;
+    private  double amount;
+    @Enumerated(EnumType.STRING)
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+    private String description;
+}
---
+++ ebanking-backend/src/main/java/com/example/ebankingbackend/web/BankAccountRestAPI.java
@@ -0,0 +1,4 @@
+package com.example.ebankingbackend.web;
+
+public class BankAccountRestAPI {
+}
